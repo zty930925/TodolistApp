e1f8e4a6a91d97a10c6e857c78b69ef84ba7435a
zty930925 <a0918772036@gmail.com>
2024-08-09 10:56:37 +0800

    Todolist_app

--- lib/daos/user_dao_local_file.dart
+++ lib/daos/user_dao_local_file.dart
@@ -8,22 +8,30 @@ import 'package:android_app_hw/models/user.dart';
 class UserDaoLocalFile {
   //取得本地儲存檔案的路徑
   Future<String> GetLocalPath() async {
+    //使用final宣告directory，讓directory實踐「路徑取得」
     final directory = await getApplicationDocumentsDirectory();
+    //回傳該路徑
     return directory.path;
   }
 
   //定義本地儲存檔案的位置
   Future<File> GetLocalFile() async {
+    //用final宣告path，調用當前的路徑取得檔案
     final path = await GetLocalPath();
+    //回傳一個資料夾，在該路徑下建立user.json
     return File('$path/user.json');
   }
 
   //從user.json中讀取User清單資料
   Future<User> readUser() async {
     try {
+      //使用final宣告file，用於取得user.json
       final file = await GetLocalFile();
+      //使用final宣告contents，用於讀取user.json字符串(dart不可用)
       final contents = await file.readAsString();
+      //使用final宣告jsonData，將字符串解碼成dart可用的資料結構
       final jsonData = jsonDecode(contents);
+      //將每個Json物件轉換為User物件並返回
       return User.fromJson(jsonData);
     } catch (e) {
       return User(totalTodos: 0, completeTodos: 0);
